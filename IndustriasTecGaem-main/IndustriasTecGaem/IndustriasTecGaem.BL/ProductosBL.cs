b7628c39e85db01f60ef7977e8b3ad630f5e95e4
MilenySp <milenysoad@gmail.com>
2022-06-07 14:12:52 -0600

    Listo

---
+++ IndustriasTecGaem-main/IndustriasTecGaem/IndustriasTecGaem.BL/ProductosBL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndustriasTecGaem.BL
+{
+    public class ProductosBL
+    {
+        Contexto _contexto;
+        public List<Producto> ListaDeProductos { get; set; }
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListaDeProductos = new List<Producto>();
+
+        }
+
+        public List<Producto> ObtenerProductos()
+        {
+            ListaDeProductos = _contexto.Productos.ToList();
+            return ListaDeProductos;
+        }
+    }
+}
---
+++ IndustriasTecGaem-main/IndustriasTecGaem/IndustriasTecGaem.Win/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IndustriasTecGaem.BL;
+
+namespace IndustriasTecGaem.Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var productosBL = new ProductosBL();
+            var listaDeProductos = productosBL.ObtenerProductos();
+
+            listaDeProductosBindingSource.DataSource = listaDeProductos;
+        }
+    }
+}
